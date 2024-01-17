fad56b14c78717c7f89f432c836b85ddb7c4b096
5ftsprout <125323803+5ftsprout@users.noreply.github.com>
2024-01-17 14:34:55 -0500

    Added solution to Convert Number to a String!

---
+++ codewars/conver-number-to-a-string.js
@@ -0,0 +1,10 @@
+//Convert a Number to a String
+function numberToString(num) {
+    return num.toString();
+  };
+
+
+//Relevant Top Answer
+function numberToString(num) {
+    return num.toString();
+  }
