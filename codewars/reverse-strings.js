898c9b4e5cfde9bbb4b0e9e4db3878386a9f7d7c
5ftsprout <125323803+5ftsprout@users.noreply.github.com>
2024-01-18 16:21:00 -0500

    Added solution to Reverse Strings

---
+++ codewars/reverse-strings.js
@@ -0,0 +1,9 @@
+//Reverse Strings
+function solution(str){
+    return str.split('').reverse().join('');
+  }
+
+//Relevant Top Answer
+function solution(str){
+    return str.split('').reverse().join('');  
+  }
