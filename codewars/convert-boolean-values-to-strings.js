785e3e472396041a72c98cc1ae873519a538610e
5ftsprout <125323803+5ftsprout@users.noreply.github.com>
2024-01-23 17:30:01 -0500

    Added solution Convert Boolean Values to Strings

---
+++ codewars/convert-boolean-values-to-strings.js
@@ -0,0 +1,10 @@
+//Convert Boolean Values to Strings
+function boolToWord( bool ){
+    return bool ? "Yes" : "No";                                  
+  };
+
+
+//Relevant Top Answer
+function boolToWord( bool ){
+    return bool ? 'Yes':'No';
+  }
