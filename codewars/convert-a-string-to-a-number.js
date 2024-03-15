4ef4e5858acaa93a3e72a35a4a131e5bf747a56b
5ftsprout <125323803+5ftsprout@users.noreply.github.com>
2024-03-15 19:47:57 -0400

    Added my solution and Relevant Top Answer to Convert a String to a Number

---
+++ codewars/convert-a-string-to-a-number.js
@@ -0,0 +1,10 @@
+//Convert a String to a Number!
+const stringToNumber = function(str){
+    return Number(str)
+  }
+
+
+// Relevant Top Answer
+var stringToNumber = function(str){
+    return parseInt(str);
+  }
