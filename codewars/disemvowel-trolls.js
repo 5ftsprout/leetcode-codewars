8d81f7274ef741ef8296900aec235b5d997c9356
5ftsprout <125323803+5ftsprout@users.noreply.github.com>
2024-03-15 17:58:44 -0400

    Added solution to Disemvowel Trolls

---
+++ codewars/disemvowel-trolls.js
@@ -0,0 +1,12 @@
+//Disemvowel Trolls
+function disemvowel(str) {
+    return str.replace(/[aeiou]/ig,'')
+  }
+  
+  disemvowel("This website is for losers LOL!")
+
+
+//Relevant Top Answer
+function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, '');
+  }
