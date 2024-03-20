91a05845470bc507775b716f86977c67c05cdac2
Annie Chhan <125323803+5ftsprout@users.noreply.github.com>
2024-03-19 22:39:52 -0400

    Added solution Count the Digit.

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 
+//Count the Digit
+function nbDig(n, d) {
+  let dMatch = 0                                            //dMatch will hold the total occurances of d up to that point.
+  for (n; n>=0; n--) {
+    const re = new RegExp(d,'g')
+    arrayD = (n**2).toString().match(re)                    //Match returns an array of all instances of d in n squared.
+    if (arrayD != null) {
+      dMatch += arrayD.length                               //Since it's an array let's get the length and add it to our variable
+    }                                                       //dMatch that is holding the amount of ds.
+  }
+  return dMatch
+}
+
+
 //Multiples of 3 or 5
 function solution(number){
   if (number <= 0) {                                        //Return 0 for 0 or negative number.
