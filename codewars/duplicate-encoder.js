2c39c4a6e83e1abef3041c3ea73de3b352132884
5ftsprout <125323803+5ftsprout@users.noreply.github.com>
2024-03-25 14:26:13 -0400

    Added retry for Duplicate Encoder.

---
+++ codewars/duplicate-encoder.js
@@ -0,0 +1,31 @@
+// Duplicate Encoder v2
+function duplicateEncode(word){
+    arr = word.toLowerCase().split('')
+    let result = ''                                                       // result will store our new string of '(' and ')' characters.
+    for (let i in arr) {
+      if (arr.filter(char => char == arr[i]).length == 1) {                // If there is only one of that character in the array proceed.
+        result += '('
+      } else {                                                            // If there is more than one of that character proceed.
+        result += ')'                                                     
+      }
+    }
+    return result
+  }
+
+
+//Duplicate Encoder
+function duplicateEncode(word){
+    wordArr = word.toLowerCase().split('')
+    const newArr = []
+    for (i = 0; i < wordArr.length; i++) {
+        let letterCount = 0
+        wordArr.forEach( (check) => wordArr[i] == check && letterCount++ && console.log(`${wordArr[i]} , ${wordArr[check]}`))
+        if (letterCount == 1) {
+            newArr.push('(')
+        } else {
+          console.log(letterCount)
+          newArr.push(')')
+        }
+    }
+    return newArr.join('')
+}
