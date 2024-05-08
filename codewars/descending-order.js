fd0ea5cc470ea16f0cc92fd035f8ce717b302499
5ftsprout <125323803+5ftsprout@users.noreply.github.com>
2024-05-08 15:37:14 -0400

    Added solution to Descending Order

---
+++ codewars/descending-order.js
@@ -0,0 +1,14 @@
+//Descending Order
+function descendingOrder(n){
+    return Number(n.toString().split('').sort().reverse().join(''))
+}
+
+
+
+
+
+
+//Top Relevant Answer
+function descendingOrder(n){
+    return parseInt(String(n).split('').sort().reverse().join(''))
+  }
